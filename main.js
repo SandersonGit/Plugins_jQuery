54963a9a04b7e61e82538351c86ac9f4b8a2f94c
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2023-10-25 21:26:58 -0300

    commit final

--- main.js
+++ main.js
@@ -8,6 +8,51 @@ $(document).ready(function(){
     })
 
 
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(00) 00000-0000'
+        // letras são representados por 's' e números '0'
+    })
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true,
+            },
+            telefone: {
+                required: true,
+            },
+            mensagem: {
+                required: true,
+            },
+            veiculo: {
+                required: false,
+            }
+        },
+        messages: {
+            nome: 'Este campo é obrigatório',
+            email: 'Email obrigatório',
+            telefone: 'Telefone obrigatório',
+            mensagem: 'Este campo é obrigatório'
+        }
+    })
+
+    $('.lista-veiculos button').click(function(){
+        const destino = $('#contato')
+        const nomeVeiculo = $(this).parent().find('h3').text()
+
+        $('#veiculo-interesse').val(nomeVeiculo)
+
+        $('html').animate({
+            scrollTop: destino.offset().top 
+        }, 1000)
+
+
+
+    })
 
 
 })
